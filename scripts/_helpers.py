31646602d09f783fffa85ed477c30753fd28c56a
Rich Chang (Yu-Chi Chang) <rich.chang.963@gmail.com>
2023-11-13 13:12:54 +0100

    add configure_logging in _helpers.py

--- scripts/_helpers.py
+++ scripts/_helpers.py
@@ -9,8 +9,49 @@ import os
 from os import PathLike
 from pathlib import Path
 from typing import Union
+import logging
 
 FilePath = Union[str, "PathLike[str]"]
+
+
+def configure_logging(snakemake, skip_handlers=False):
+    """Configure the basic behaviour for the logging module.
+    Note: Must only be called once from the __main__ section of a script.
+    The setup includes printing log messages to STDERR and to a log file defined
+    by either (in priority order): snakemake.log.python, snakemake.log[0] or "logs/{rulename}.log".
+    Additional keywords from logging.basicConfig are accepted via the snakemake configuration
+    file under snakemake.config.logging.
+
+    Parameters
+    ----------
+    snakemake : Snakemake object
+        Your snakemake object containing a snakemake.config and snakemake.log.
+    skip_handlers : boolean, optional
+        Do (not) skip the default handlers created for redirecting output to STDERR and file.
+        Default by False
+    """
+    kwargs = snakemake.config.get("logging", dict())
+    kwargs.setdefault("level", "INFO")
+
+    if skip_handlers is False:
+        fallback_path = Path(__file__).parent.joinpath(
+            "..", "logs", f"{snakemake.rule}.log"
+        )
+        logfile = snakemake.log.get(
+            "python", snakemake.log[0] if snakemake.log else fallback_path
+        )
+        kwargs.update(
+            {
+                "handlers": [
+                    # Prefer the 'python' log, otherwise take the first log for each
+                    # Snakemake rule
+                    logging.FileHandler(logfile),
+                    logging.StreamHandler(),
+                ]
+            }
+        )
+    logging.basicConfig(**kwargs)
+
 
 def mock_snakemake(rulename, **wildcards):
     """This function is expected to be executed from the 'scripts'-directory of '
