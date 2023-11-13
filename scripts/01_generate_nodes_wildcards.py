88491ffaea410e3c2e0ba6b005d086c3e6e37ef3
Rich Chang (Yu-Chi Chang) <rich.chang.963@gmail.com>
2023-11-13 13:10:55 +0100

    add 01_generate_nodes_wildcards.py

---
+++ scripts/01_generate_nodes_wildcards.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: : 2022-2023 The PyPSA-Eur and Agora Energiewende Authors
+
+# SPDX-License-Identifier: MIT
+
+# coding: utf-8
+
+import os
+# extra pre-setting for geopandas to exclude unclosed rings in polygons
+os.environ["USE_PYGEOS"] = "0"
+os.environ["OGR_GEOMETRY_ACCEPT_UNCLOSED_RING"] = "NO"
+import geopandas as gpd
+from _helpers import FilePath, configure_logging
+
+
+def clean_node(nuts: FilePath):
+    """Save only region name and node_ids into pd.DataFrame
+
+    Parameters
+    ----------
+    nuts : FilePath
+        Input geojson file of nuts level
+    excl_iso_list: List, optional
+        Exclude specific countries. Default by None
+    country: string, optional
+        Filter specific country. Default by None
+    """
+    nodes = gpd.read_file(nuts)
+
+    nodes = nodes[["id", "na"]].rename(columns={"na": "node"})
+
+    nodes.sort_values(by="id", ascending=True).sort_index().to_json(snakemake.output.node_wildcards)
+
+
+if __name__ == "__main__":
+    if "snakemake" not in globals():
+        from _helpers import mock_snakemake
+
+        snakemake = mock_snakemake("generate_wildcards")
+    configure_logging(snakemake)
+    
+    clean_node(nuts=snakemake.input.nodes)
+
+
