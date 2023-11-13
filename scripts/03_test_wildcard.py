d4d007db180106ffa3810dc704f99d3201e52806
Rich Chang (Yu-Chi Chang) <rich.chang.963@gmail.com>
2023-11-13 13:37:11 +0100

    add 03_test_wildcard.py

---
+++ scripts/03_test_wildcard.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+import logging
+
+import pandas as pd
+import numpy as np
+import pypsa
+import warnings
+from _helpers import FilePath, configure_logging
+
+logger = logging.getLogger(__name__)
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+
+
+if __name__ == "__main__":                
+    if "snakemake" not in globals():
+        from _helpers import mock_snakemake
+        
+        snakemake = mock_snakemake("solve_network", nodes="DEU_131")
+    configure_logging(snakemake)
+
+    node_id = snakemake.wildcards.nodes
+    print(node_id)
